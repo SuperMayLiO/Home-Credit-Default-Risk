18d25891b522d7fea8d2e107bb109a1cd6faf631
mayritaspring <mayritaspring@gmail.com>
2018-06-10 16:37:43 +0800

    add grid search to home1 and add home2

---
+++ home2.py
@@ -0,0 +1,324 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 10 16:09:03 2018
+
+@author: user
+"""
+
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import roc_auc_score, precision_recall_curve, roc_curve
+from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
+import seaborn as sns
+import gc
+import os
+import sys
+
+
+path = os.path.abspath(os.path.dirname(sys.argv[0])) #set path in same folder
+path = "C:/Users/r05h41009/Documents/May/Home-Credit-Default-Risk/input"
+gc.enable()
+
+buro_bal = pd.read_csv(path + '/input/bureau_balance.csv')
+print('Buro bal shape : ', buro_bal.shape)
+
+print('transform to dummies')
+buro_bal = pd.concat([buro_bal, pd.get_dummies(buro_bal.STATUS, prefix='buro_bal_status')], axis=1).drop('STATUS', axis=1)
+
+print('Counting buros')
+buro_counts = buro_bal[['SK_ID_BUREAU', 'MONTHS_BALANCE']].groupby('SK_ID_BUREAU').count()
+buro_bal['buro_count'] = buro_bal['SK_ID_BUREAU'].map(buro_counts['MONTHS_BALANCE'])
+
+print('averaging buro bal')
+avg_buro_bal = buro_bal.groupby('SK_ID_BUREAU').mean()
+
+avg_buro_bal.columns = ['avg_buro_' + f_ for f_ in avg_buro_bal.columns]
+del buro_bal
+gc.collect()
+
+print('Read Bureau')
+buro = pd.read_csv(path + '/input/bureau.csv')
+
+print('Go to dummies')
+buro_credit_active_dum = pd.get_dummies(buro.CREDIT_ACTIVE, prefix='ca_')
+buro_credit_currency_dum = pd.get_dummies(buro.CREDIT_CURRENCY, prefix='cu_')
+buro_credit_type_dum = pd.get_dummies(buro.CREDIT_TYPE, prefix='ty_')
+
+buro_full = pd.concat([buro, buro_credit_active_dum, buro_credit_currency_dum, buro_credit_type_dum], axis=1)
+# buro_full.columns = ['buro_' + f_ for f_ in buro_full.columns]
+
+del buro_credit_active_dum, buro_credit_currency_dum, buro_credit_type_dum
+gc.collect()
+
+print('Merge with buro avg')
+buro_full = buro_full.merge(right=avg_buro_bal.reset_index(), how='left', on='SK_ID_BUREAU', suffixes=('', '_bur_bal'))
+
+print('Counting buro per SK_ID_CURR')
+nb_bureau_per_curr = buro_full[['SK_ID_CURR', 'SK_ID_BUREAU']].groupby('SK_ID_CURR').count()
+buro_full['SK_ID_BUREAU'] = buro_full['SK_ID_CURR'].map(nb_bureau_per_curr['SK_ID_BUREAU'])
+
+print('Averaging bureau')
+avg_buro = buro_full.groupby('SK_ID_CURR').mean()
+print(avg_buro.head())
+
+del buro, buro_full
+gc.collect()
+
+print('Read prev')
+prev = pd.read_csv(path + '/input/previous_application.csv')
+
+prev_cat_features = [
+    f_ for f_ in prev.columns if prev[f_].dtype == 'object'
+]
+
+print('Go to dummies')
+prev_dum = pd.DataFrame()
+for f_ in prev_cat_features:
+    prev_dum = pd.concat([prev_dum, pd.get_dummies(prev[f_], prefix=f_).astype(np.uint8)], axis=1)
+
+prev = pd.concat([prev, prev_dum], axis=1)
+
+del prev_dum
+gc.collect()
+
+print('Counting number of Prevs')
+nb_prev_per_curr = prev[['SK_ID_CURR', 'SK_ID_PREV']].groupby('SK_ID_CURR').count()
+prev['SK_ID_PREV'] = prev['SK_ID_CURR'].map(nb_prev_per_curr['SK_ID_PREV'])
+
+print('Averaging prev')
+avg_prev = prev.groupby('SK_ID_CURR').mean()
+print(avg_prev.head())
+del prev
+gc.collect()
+
+print('Reading POS_CASH')
+pos = pd.read_csv(path + '/input/POS_CASH_balance.csv')
+
+print('Go to dummies')
+pos = pd.concat([pos, pd.get_dummies(pos['NAME_CONTRACT_STATUS'])], axis=1)
+
+print('Compute nb of prevs per curr')
+nb_prevs = pos[['SK_ID_CURR', 'SK_ID_PREV']].groupby('SK_ID_CURR').count()
+pos['SK_ID_PREV'] = pos['SK_ID_CURR'].map(nb_prevs['SK_ID_PREV'])
+
+print('Go to averages')
+avg_pos = pos.groupby('SK_ID_CURR').mean()
+
+del pos, nb_prevs
+gc.collect()
+
+print('Reading CC balance')
+cc_bal = pd.read_csv(path + '/input/credit_card_balance.csv')
+
+print('Go to dummies')
+cc_bal = pd.concat([cc_bal, pd.get_dummies(cc_bal['NAME_CONTRACT_STATUS'], prefix='cc_bal_status_')], axis=1)
+
+nb_prevs = cc_bal[['SK_ID_CURR', 'SK_ID_PREV']].groupby('SK_ID_CURR').count()
+cc_bal['SK_ID_PREV'] = cc_bal['SK_ID_CURR'].map(nb_prevs['SK_ID_PREV'])
+
+print('Compute average')
+avg_cc_bal = cc_bal.groupby('SK_ID_CURR').mean()
+avg_cc_bal.columns = ['cc_bal_' + f_ for f_ in avg_cc_bal.columns]
+
+del cc_bal, nb_prevs
+gc.collect()
+
+print('Reading Installments')
+inst = pd.read_csv(path + '/input/installments_payments.csv')
+nb_prevs = inst[['SK_ID_CURR', 'SK_ID_PREV']].groupby('SK_ID_CURR').count()
+inst['SK_ID_PREV'] = inst['SK_ID_CURR'].map(nb_prevs['SK_ID_PREV'])
+
+avg_inst = inst.groupby('SK_ID_CURR').mean()
+avg_inst.columns = ['inst_' + f_ for f_ in avg_inst.columns]
+
+print('Read data and test')
+data = pd.read_csv(path + '/input/application_train.csv')
+test = pd.read_csv(path + '/input/application_test.csv')
+print('Shapes : ', data.shape, test.shape)
+
+y = data['TARGET']
+del data['TARGET']
+
+categorical_feats = [
+    f for f in data.columns if data[f].dtype == 'object'
+]
+categorical_feats
+for f_ in categorical_feats:
+    data[f_], indexer = pd.factorize(data[f_])
+    test[f_] = indexer.get_indexer(test[f_])
+    
+data = data.merge(right=avg_buro.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_buro.reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(right=avg_prev.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_prev.reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(right=avg_pos.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_pos.reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(right=avg_cc_bal.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_cc_bal.reset_index(), how='left', on='SK_ID_CURR')
+
+data = data.merge(right=avg_inst.reset_index(), how='left', on='SK_ID_CURR')
+test = test.merge(right=avg_inst.reset_index(), how='left', on='SK_ID_CURR')
+
+del avg_buro, avg_prev
+gc.collect()
+
+from lightgbm import LGBMClassifier
+import gc
+
+gc.enable()
+
+folds = KFold(n_splits=5, shuffle=True, random_state=527)
+oof_preds = np.zeros(data.shape[0])
+sub_preds = np.zeros(test.shape[0])
+
+feature_importance_df = pd.DataFrame()
+
+feats = [f for f in data.columns if f not in ['SK_ID_CURR']]
+
+#select best parameter
+from __future__ import print_function
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+print(__doc__)
+
+X_train, y_train = data[feats].iloc[trn_idx], y.iloc[trn_idx]
+X_test, y_test = data[feats].iloc[val_idx], y.iloc[val_idx]
+
+tuned_parameters = [{'n_estimators': [3000,4000,5000],
+                     'learning_rate': [0.02,0.03,0.04],
+                     'num_leaves': [20,30,40],
+                     'colsample_bytree': [0.7,0.8,0.9],
+                     'subsample': [0.8,0.9,1],
+                     'max_depth': [6,7,8],
+                     'reg_alpha': [0,0.1,0.2],
+                     'reg_lambda': [0,0.1,0.2],
+                     'min_split_gain': [0,0.01,0.02],
+                     'min_child_weight': [1,2,3],
+                     'silent': [-2,-1,0],
+                     'verbose': [-2,-1,0]}]
+
+scores = ['precision', 'recall']
+
+for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+
+    clf = GridSearchCV(LGBMClassifier(), tuned_parameters, cv=5,
+                       scoring='%s_macro' % score)
+    clf.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
+
+
+#use best model
+for n_fold, (trn_idx, val_idx) in enumerate(folds.split(data)):
+    trn_x, trn_y = data[feats].iloc[trn_idx], y.iloc[trn_idx]
+    val_x, val_y = data[feats].iloc[val_idx], y.iloc[val_idx]
+    
+    clf = LGBMClassifier(clf.best_params_)
+    clf.fit(trn_x, trn_y, 
+            eval_set= [(trn_x, trn_y), (val_x, val_y)], 
+            eval_metric='auc', verbose=100, early_stopping_rounds=100  #30
+           )
+    
+    oof_preds[val_idx] = clf.predict_proba(val_x, num_iteration=clf.best_iteration_)[:, 1]
+    sub_preds += clf.predict_proba(test[feats], num_iteration=clf.best_iteration_)[:, 1] / folds.n_splits
+    
+    fold_importance_df = pd.DataFrame()
+    fold_importance_df["feature"] = feats
+    fold_importance_df["importance"] = clf.feature_importances_
+    fold_importance_df["fold"] = n_fold + 1
+    feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
+    
+    print('Fold %2d AUC : %.6f' % (n_fold + 1, roc_auc_score(val_y, oof_preds[val_idx])))
+    del clf, trn_x, trn_y, val_x, val_y
+    gc.collect()
+    
+print('Full AUC score %.6f' % roc_auc_score(y, oof_preds)) 
+
+test['TARGET'] = sub_preds
+
+test[['SK_ID_CURR', 'TARGET']].to_csv('first_submission.csv', index=False)
+
+# Plot feature importances
+cols = feature_importance_df[["feature", "importance"]].groupby("feature").mean().sort_values(
+    by="importance", ascending=False)[:50].index
+
+best_features = feature_importance_df.loc[feature_importance_df.feature.isin(cols)]
+
+plt.figure(figsize=(8,10))
+sns.barplot(x="importance", y="feature", data=best_features.sort_values(by="importance", ascending=False))
+plt.title('LightGBM Features (avg over folds)')
+plt.tight_layout()
+plt.savefig('lgbm_importances.png')
+
+# Plot ROC curves
+plt.figure(figsize=(6,6))
+scores = [] 
+for n_fold, (_, val_idx) in enumerate(folds.split(data)):  
+    # Plot the roc curve
+    fpr, tpr, thresholds = roc_curve(y.iloc[val_idx], oof_preds[val_idx])
+    score = roc_auc_score(y.iloc[val_idx], oof_preds[val_idx])
+    scores.append(score)
+    plt.plot(fpr, tpr, lw=1, alpha=0.3, label='ROC fold %d (AUC = %0.4f)' % (n_fold + 1, score))
+
+plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Luck', alpha=.8)
+fpr, tpr, thresholds = roc_curve(y, oof_preds)
+score = roc_auc_score(y, oof_preds)
+plt.plot(fpr, tpr, color='b',
+         label='Avg ROC (AUC = %0.4f $\pm$ %0.4f)' % (score, np.std(scores)),
+         lw=2, alpha=.8)
+
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('LightGBM ROC Curve')
+plt.legend(loc="lower right")
+plt.tight_layout()
+
+plt.savefig('roc_curve.png')
+
+# Plot ROC curves
+plt.figure(figsize=(6,6))
+precision, recall, thresholds = precision_recall_curve(y, oof_preds)
+score = roc_auc_score(y, oof_preds)
+plt.plot(precision, recall, color='b',
+         label='Avg ROC (AUC = %0.4f $\pm$ %0.4f)' % (score, np.std(scores)),
+         lw=2, alpha=.8)
+
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('LightGBM Recall / Precision')
+plt.legend(loc="best")
+plt.tight_layout()
+
+plt.savefig('recall_precision_curve.png')
