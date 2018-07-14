3923fb5469f3276dd462006e934a6ee9fcae18af
a4455669 <38220099+a4455669@users.noreply.github.com>
2018-07-14 17:52:15 +0800

    handle TARGET columns in one hot encoding

--- home4.py
+++ home4.py
@@ -25,6 +25,7 @@ def timer(title):
 def one_hot_encoder(df, nan_as_category = True):
     original_columns = list(df.columns)
     categorical_columns = [col for col in df.columns if df[col].dtype == 'object' or len(df[col].unique().tolist()) < 20]
+    if ("TARGET" in categorical_columns): categorical_columns.remove('TARGET')
     df = pd.get_dummies(df, columns= categorical_columns, dummy_na= nan_as_category)
     new_columns = [c for c in df.columns if c not in original_columns]
     return df, new_columns
